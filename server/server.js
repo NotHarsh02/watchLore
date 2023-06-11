cb4d6dea479d3e15b9a3153b00b7ba6391240891
unknown <harshthegreat2002@gmail.com>
2023-06-11 20:37:35 +0530

    d

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ const sessionConfig = {
     cookie: {
         
         httpOnly: true,
-        // sameSite: "none",
+        sameSite: "none",
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
