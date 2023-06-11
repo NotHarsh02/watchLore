2415cec7023e2451071d4fbb0839888b853072cd
unknown <harshthegreat2002@gmail.com>
2023-06-11 22:02:48 +0530

    11thjune

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 if (process.env.type === 'production') {
   app.use(cors({
    
-    origin: 'https://fancy-trifle-61ead3.netlify.app/', // replace with the domain of your React app
+    origin: 'https://fancy-trifle-61ead3.netlify.app', // replace with the domain of your React app
     credentials: true
   }));
 } else {
